965fa9db9572fd46f88a2c89af52bba2f6b5f085
yonatang <yonatan.gelbard@comm-it.com>
2018-10-05 08:49:12 +0300

    basic http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
@@ -28,7 +28,8 @@ import { MainTraineeComponent } from './components/trainee/main-trainee/main-tra
     MainTraineeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/httpService/http.service.ts
+++ src/app/services/httpService/http.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from 'selenium-webdriver/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  get(url: string): Observable<any> {
+    return new Observable<any>();
+  }
+
+  post(url: string, body = {}): Observable<any> {
+    return new Observable<any>();
+  }
+
+  put(url: string, body = {}): Observable<any> {
+    return new Observable<any>();
+  }
+
+  delete(url: string): Observable<any> {
+    return new Observable<any>();
+  }
+
 }
